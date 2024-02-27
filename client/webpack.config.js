d8e22dcc5070894ab84a8bf3e20bc580465d07d7
Kevin Hogan <kchogan@pacbell.net>
2024-02-26 17:11:27 -0800

    webpack config fix

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,6 +42,7 @@ module.exports = () => {
           {
             src: path.resolve('./src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
           },
         ],
       }),      
